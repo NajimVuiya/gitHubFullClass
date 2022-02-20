15094c153c1a68994d6ed35c11614f5c03e76895
Gias uddin vuiya <giasuddin6900@gmail.com>
2022-02-21 00:04:09 +0600

    This is first virson - 0.0

---
+++ js/script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+
+
+function formValidation(){
+	let userName = 'Najim vuiya';
+	let emailAddress = 'najim@gmail.com';
+	if(userName === 'Najim vuiya' && emailAddress === 'najim@gmail.com')
+	{
+		console.log('Your Registation has been sucessfully');
+	}else 
+	{
+		console.log('Please try again');
+	}
+}
+formValidation()
